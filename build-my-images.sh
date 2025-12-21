7ec56a9ab1fdb880490d54fa9badd6fe6be6c37b
Michael Morgan <mjmorgan66@gmail.com>
2025-12-21 09:48:41 -0600

    working module for guild tax, but needs cleanup

--- build-my-images.sh
+++ build-my-images.sh
@@ -8,16 +8,16 @@
 docker compose build
 
 #now build my init image for k3s
-DATA_INIT_IMAGE=registry.home/wow/wotlk-env-data-import-playerbots-with-auctionhouse:1.0
+DATA_INIT_IMAGE=registry.home/wow/wotlk-env-data-import-playerbots-with-auctionhouse:1.1
 
 docker build . -f Dockerfile-db-init -t $DATA_INIT_IMAGE
 #docker push $DATA_INIT_IMAGE
 
 # TODO: manual names/tagging...make variables
 # push everything to my repository
-docker push registry.home/wow/ac-wotlk-authserver-playerbots-with-auctionhouse:1.0
-docker push registry.home/wow/ac-wotlk-worldserver-playerbots-with-auctionhouse:1.0
-docker push registry.home/wow/ac-wotlk-client-data-playerbots-with-auctionhouse:1.0
-docker push registry.home/wow/wotlk-env-db-import-playerbots-with-auctionhouse:1.0
-docker push registry.home/wow/wotlk-env-data-import-playerbots-with-auctionhouse:1.0
+docker push registry.home/wow/ac-wotlk-authserver-playerbots-with-auctionhouse:1.1
+docker push registry.home/wow/ac-wotlk-worldserver-playerbots-with-auctionhouse:1.1
+docker push registry.home/wow/ac-wotlk-client-data-playerbots-with-auctionhouse:1.1
+docker push registry.home/wow/wotlk-env-db-import-playerbots-with-auctionhouse:1.1
+docker push registry.home/wow/wotlk-env-data-import-playerbots-with-auctionhouse:1.1
 docker push registry.home/wow/mysql:8.4 # a default mysql image
