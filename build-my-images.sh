56b11fef6f1e0777156fa34d5242b8f1a6c374cd
Michael Morgan <mjmorgan66@gmail.com>
2026-03-23 19:04:24 -0500

    updated version

--- build-my-images.sh
+++ build-my-images.sh
@@ -8,7 +8,7 @@
 docker compose build
 
 #now build my init image for k3s
-DATA_INIT_IMAGE=registry.home/wow/wotlk-env-data-import-playerbots-with-auctionhouse:1.2.5.2
+DATA_INIT_IMAGE=registry.home/wow/wotlk-env-data-import-playerbots-with-auctionhouse:1.3
 
 docker build . -f Dockerfile-db-init -t $DATA_INIT_IMAGE
 #docker push $DATA_INIT_IMAGE
@@ -16,9 +16,9 @@ docker build . -f Dockerfile-db-init -t $DATA_INIT_IMAGE
 # TODO: manual names/tagging...make variables
 # push everything to my repository
 # DISABLED so that i could only work on and update the db import image, otherwise this would auto push all images
-docker push registry.home/wow/ac-wotlk-authserver-playerbots-with-auctionhouse:1.2.5.2
-docker push registry.home/wow/ac-wotlk-worldserver-playerbots-with-auctionhouse:1.2.5.2
-docker push registry.home/wow/ac-wotlk-client-data-playerbots-with-auctionhouse:1.2.5.2
-docker push registry.home/wow/wotlk-env-db-import-playerbots-with-auctionhouse:1.2.5.2
-docker push registry.home/wow/wotlk-env-data-import-playerbots-with-auctionhouse:1.2.5.2
+docker push registry.home/wow/ac-wotlk-authserver-playerbots-with-auctionhouse:1.3
+docker push registry.home/wow/ac-wotlk-worldserver-playerbots-with-auctionhouse:1.3
+docker push registry.home/wow/ac-wotlk-client-data-playerbots-with-auctionhouse:1.3
+docker push registry.home/wow/wotlk-env-db-import-playerbots-with-auctionhouse:1.3
+docker push registry.home/wow/wotlk-env-data-import-playerbots-with-auctionhouse:1.3
 #docker push registry.home/wow/mysql:8.4 # a default mysql image
